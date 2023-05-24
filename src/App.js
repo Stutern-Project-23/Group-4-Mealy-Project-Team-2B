9280412d6872ed1049e79b27c00cfbb61a30c575
Gelaraph <angelaraph12@gmail.com>
2023-05-24 10:12:58 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line import/no-unresolved
+import NavBar from "component/NavBar";
+import "./App.css";
+
+const App = () => (
+  <div className="App">
+    <NavBar />
+  </div>
+);
+
+export default App;

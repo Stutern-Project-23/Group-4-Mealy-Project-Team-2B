bc6da9f4a8cf2a91ada5f969b7d001404a64706a
KasiePatricia <ugwukasiep@gmail.com>
2023-05-26 08:45:26 +0100

    Added Reset Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 // eslint-disable-next-line import/no-unresolved
 import NavBar from "component/NavBar";
 import "./App.css";
+import Reset from "pages/Reset/Reset";
+import GlobalStyle from "styles/global";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => (
-  <div className="App">
+  <BrowserRouter>
+    <GlobalStyle />
     <NavBar />
-  </div>
+    <Routes>
+      <Route path="/reset" element={<Reset />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;

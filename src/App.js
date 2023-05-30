3edd7ac8d38f0f7e855cd8fe95376f37ce49fa0a
Gelaraph <angelaraph12@gmail.com>
2023-05-30 15:44:48 +0100

    added form validations

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
 import { Routes, Route } from "react-router-dom";
 import VerifyEmail from "./pages/forgotPassword/VerifyEmail";
+import ValidationProvider from "./hooks/FormValidationsContext";
 import SignUp from "./pages/signUp/SignUp";
 import Home from "./pages/Home";
 import SignUpVerification from "./pages/signUpVerification/SignUpVerification";
-import ResetPassword from "./pages/resetPassword/ResetPassword";
 import ResetPasswordEmailVerification from "./pages/resetPassword/ResetPasswordEmailVerification";
 import "./App.css";
-import NewPassword from "./pages/forgotPassword/NewPassword";
 import ForgotPassword from "./pages/forgotPassword/ForgotPassword";
+import NewPassword from "./pages/forgotPassword/NewPassword";
+import ResetPassword from "./pages/resetPassword/ResetPassword";
 import SignIn from "./pages/signIn/SignIn";
 
 const App = () => (
   <div className="App">
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="sign-up-verification" element={<SignUpVerification />} />
-      <Route path="sign-up" element={<SignUp />} />
-      <Route path="forgot-password" element={<ForgotPassword />} />
-      <Route path="forgot-password-verification" element={<VerifyEmail />} />
-      <Route path="create-new-password" element={<NewPassword />} />
-      <Route path="reset-password" element={<ResetPassword />} />
-      <Route path="sign-in" element={<SignIn />} />
-      <Route
-        path="reset-password-verification"
-        element={<ResetPasswordEmailVerification />}
-      />
-    </Routes>
+    <ValidationProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="sign-up-verification" element={<SignUpVerification />} />
+        <Route path="sign-up" element={<SignUp />} />
+        <Route path="forgot-password" element={<ForgotPassword />} />
+        <Route path="forgot-password-verification" element={<VerifyEmail />} />
+        <Route path="create-new-password" element={<NewPassword />} />
+        <Route path="reset-password" element={<ResetPassword />} />
+        <Route path="sign-in" element={<SignIn />} />
+        <Route
+          path="reset-password-verification"
+          element={<ResetPasswordEmailVerification />}
+        />
+      </Routes>
+    </ValidationProvider>
   </div>
 );
 

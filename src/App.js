d6e12fc327358d27975262483a505a8d5038af1f
Theresah9979 <theresahb14@gmail.com>
2023-06-02 01:52:35 -0700

    payment checkout page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import { Routes, Route } from "react-router-dom";
-import VerifyEmail from "./pages/forgotPassword/VerifyEmail";
+// import VerifyEmail from "./pages/forgotPassword/VerifyEmail";
 import ValidationProvider from "./hooks/FormValidationsContext";
 import SignUp from "./pages/signUp/SignUp";
 import Home from "./pages/Home";
 import SignUpVerification from "./pages/signUpVerification/SignUpVerification";
 import ResetPasswordEmailVerification from "./pages/resetPassword/ResetPasswordEmailVerification";
 import "./App.css";
-import ForgotPassword from "./pages/forgotPassword/ForgotPassword";
-import NewPassword from "./pages/forgotPassword/NewPassword";
+// import ForgotPassword from "./pages/forgotPassword/ForgotPassword";
+// import NewPassword from "./pages/forgotPassword/NewPassword";
 import ResetPassword from "./pages/resetPassword/ResetPassword";
 import SignIn from "./pages/signIn/SignIn";
+import PaymentCheckout from "./pages/paymentCheckout/PaymentCheckout";
 
 const App = () => (
   <div className="App">
+    <PaymentCheckout />
     <ValidationProvider>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="sign-up-verification" element={<SignUpVerification />} />
         <Route path="sign-up" element={<SignUp />} />
-        <Route path="forgot-password" element={<ForgotPassword />} />
+        {/* <Route path="forgot-password" element={<ForgotPassword />} />
         <Route path="forgot-password-verification" element={<VerifyEmail />} />
-        <Route path="create-new-password" element={<NewPassword />} />
+<Route path="create-new-password" element={<NewPassword />} /> */}
         <Route path="reset-password" element={<ResetPassword />} />
         <Route path="sign-in" element={<SignIn />} />
         <Route

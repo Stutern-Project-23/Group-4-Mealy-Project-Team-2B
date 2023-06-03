a0424160e44cbf0083b201e774723b3471f82872
KasiePatricia <ugwukasiep@gmail.com>
2023-06-03 18:21:41 +0100

    Hero section and payment confirmation

---
+++ src/Data/HeroSectionData.js
@@ -0,0 +1,38 @@
+import AfricanImg from "../assets/images/hero/african.png";
+import HealthyImg from "../assets/images/hero/healthy.png";
+import SnacksImg from "../assets/images/hero/snacks.png";
+
+const CategoryData = [
+  {
+    id: 1,
+    categoryImg: HealthyImg,
+    title: "Healthy",
+  },
+  {
+    id: 2,
+    categoryImg: HealthyImg,
+    title: "Healthy",
+  },
+  {
+    id: 3,
+    categoryImg: AfricanImg,
+    title: "African",
+  },
+  {
+    id: 4,
+    categoryImg: AfricanImg,
+    title: "African",
+  },
+  {
+    id: 5,
+    categoryImg: SnacksImg,
+    title: "Snacks",
+  },
+  {
+    id: 6,
+    categoryImg: AfricanImg,
+    title: "African",
+  },
+];
+
+export default CategoryData;

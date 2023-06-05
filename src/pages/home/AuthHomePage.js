b2f6c1323db29e7ba732fd90ac39c4bbd43109cc
Gelaraph <angelaraph12@gmail.com>
2023-06-05 15:23:07 +0100

    fixed bugs

--- src/pages/home/AuthHomePage.js
+++ src/pages/home/AuthHomePage.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Layout from "../../component/Layout";
-// import MealCustomizationComponentWrapper from "../../component/mealCustomization/MealCustomizationComponentWrapper";
+import MealCustomizationComponentWrapper from "../../component/mealCustomization/MealCustomizationComponentWrapper";
+import HeroSection from "../../component/authComp/HeroSection/HeroSection";
 
 const AuthHomePage = () => (
   <Layout>
-    AuthHomePage
-    {/* <MealCustomization /> */}
+    <HeroSection />
+    <MealCustomizationComponentWrapper />
   </Layout>
 );
 
--- src/pages/home/GuestHomePage.js
+++ src/pages/home/GuestHomePage.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Layout from "../../component/Layout";
 import MealCustomizationComponentWrapper from "../../component/mealCustomization/MealCustomizationComponentWrapper";
+import HeroSection from "../../component/authComp/HeroSection/HeroSection";
 
 const GuestHomePage = () => (
   <Layout>
-    GuestHomePage
+    <HeroSection />
     <MealCustomizationComponentWrapper />
   </Layout>
 );
